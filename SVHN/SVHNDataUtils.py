b427054ef1fbe1d09a6c45929fd06927b15bc70d
Kashyap Chitta <kashyap.chitta@gmail.com>
2017-10-18 12:10:19 -0400

    Fixes
    Preprocessing bugfixes

--- SVHN/SVHNDataUtils.py
+++ SVHN/SVHNDataUtils.py
@@ -4,39 +4,49 @@ import h5py
 import keras
 
 def getSVHNData():
-	num_classes = 10
-	svhnTrain = sio.loadmat('train_32x32.mat')
-	svhnTest = sio.loadmat('test_32x32.mat')
+    num_classes = 10
+    svhnTrain = sio.loadmat('train_32x32.mat')
+    svhnTest = sio.loadmat('test_32x32.mat')
 
-	xTrain = svhnTrain['X']
-	xTest = svhnTest['X']
-	yTrain = svhnTrain['y']
-	yTest = svhnTest['y']
+    xTrain = svhnTrain['X']
+    xTest = svhnTest['X']
+    yTrain = svhnTrain['y']
+    yTest = svhnTest['y']
 
-	# input image dimensions
-	img_rows, img_cols = 32, 32
+    # input image dimensions
+    img_rows, img_cols = 32, 32
 
-	x_train = xTrain
-	y_train = yTrain
-	x_test = xTest
-	y_test = yTest
+    x_train = xTrain
+    y_train = yTrain
+    x_test = xTest
+    y_test = yTest
 
-	# trainMean = np.mean(x_train, axis=0)
+    x_train = np.transpose(x_train, (3, 0, 1, 2))
+    x_test = np.transpose(x_test, (3, 0, 1, 2))
 
-	trainMeanFile = h5py.File('SVHN_train_mean.h5', 'r')
-	# trainMeanFile.create_dataset('train_mean', data=trainMean)
-	trainMean = np.array(trainMeanFile['train_mean'])
-	trainMeanFile.close()
+    '''
+    trainMean = np.mean(x_train, axis=0)
+    print(trainMean)
+    trainSigma = np.std(x_train, axis=0)
+    print(trainSigma)
+    '''
 
-	x_train = np.transpose(x_train, (3, 0, 1, 2)) - trainMean
-	x_test = np.transpose(x_test, (3, 0, 1, 2)) - trainMean
+    trainMeanFile = h5py.File('SVHN_preprocessing.h5', 'r')
+    #trainMeanFile.create_dataset('train_mean', data=trainMean)
+    #trainMeanFile.create_dataset('train_sigma', data=trainSigma)
+    trainMean = np.array(trainMeanFile['train_mean'])
+    trainSigma = np.array(trainMeanFile['train_sigma'])
+    trainMeanFile.close()
 
-	# convert class vectors to binary class matrices
-	y_train -= 1
-	y_test -= 1
-	y_train = y_train.ravel()
-	y_test = y_test.ravel()
-	y_train = keras.utils.np_utils.to_categorical(y_train, num_classes)
-	y_test = keras.utils.np_utils.to_categorical(y_test, num_classes)
+    x_train = (x_train - trainMean)/trainSigma
+    x_test = (x_test - trainMean)/trainSigma
 
-	return x_train, y_train, x_test, y_test
+    # convert class vectors to binary class matrices
+    y_train -= 1
+    y_test -= 1
+    y_train = y_train.ravel()
+    y_test = y_test.ravel()
+    y_train = keras.utils.np_utils.to_categorical(y_train, num_classes)
+    y_test = keras.utils.np_utils.to_categorical(y_test, num_classes)
+
+    return x_train, y_train, x_test, y_test
--- SVHN/SVHN_CNN.py
+++ SVHN/SVHN_CNN.py
@@ -7,16 +7,16 @@ from denseBlocks import DenseNet, wideResNet
 from SVHNDataUtils import getSVHNData
 from visualization import *
 
-batch_size = 128
+batch_size = 32
 epochs = 20
 
 x_train, y_train, x_test, y_test = getSVHNData()
 
-model = wideResNet(k=8, dropout=0.4, include_target='false')
-#model = DenseNet(growth_rate=48, reduction=0.5, dropout_rate=0.0, include_target='false')
+#model = wideResNet(k=8, dropout=0.4, include_target='false')
+model = DenseNet(growth_rate=48, reduction=0.5, dropout_rate=0.2, include_target='false')
 
 # Optimizer
-adam = Adam(lr= 0.001, beta_1= 0.9, beta_2= 0.999, epsilon= 1e-08, decay= 0.0)
+adam = Adam(lr= 0.01, beta_1= 0.9, beta_2= 0.999, epsilon= 1e-08, decay= 0.0)
 
 model.compile(loss=keras.losses.categorical_crossentropy,
               optimizer=adam,
