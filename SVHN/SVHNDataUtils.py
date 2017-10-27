5b7ba5c282762b6807a3253200e9a36a9ccdc0fc
Kashyap Chitta <kashyap.chitta@gmail.com>
2017-10-27 13:40:35 -0400

    Fixes
    Fixed SVHN data preperation procedure

--- SVHN/SVHNDataUtils.py
+++ SVHN/SVHNDataUtils.py
@@ -6,47 +6,35 @@ import keras
 def getSVHNData():
     num_classes = 10
     svhnTrain = sio.loadmat('train_32x32.mat')
+    svhnExtra = sio.loadmat('extra_32x32.mat')
     svhnTest = sio.loadmat('test_32x32.mat')
 
-    xTrain = svhnTrain['X']
-    xTest = svhnTest['X']
-    yTrain = svhnTrain['y']
-    yTest = svhnTest['y']
+    mask1 = range(69257, 73257)
+    mask2 = range(69257)
+    mask3 = range(529131, 531131)
+    mask4 = range(529131)
+
+    x_train = np.concatenate([svhnTrain['X'][mask2],svhnExtra['X'][mask4]],axis=3)/255
+    x_val = np.concatenate([svhnTrain['X'][mask1],svhnExtra['X'][mask3]],axis=3)/255
+    x_test = svhnTest['X']/255
+    y_train = np.concatenate([svhnTrain['y'][mask2],svhnExtra['y'][mask4]], axis=0) - 1
+    y_val = np.concatenate([svhnTrain['y'][mask1], svhnExtra['y'][mask3]], axis=0) - 1
+    y_test = svhnTest['y'] - 1
 
     # input image dimensions
     img_rows, img_cols = 32, 32
-
-    x_train = xTrain
-    y_train = yTrain
-    x_test = xTest
-    y_test = yTest
+    print (y_val)
 
     x_train = np.transpose(x_train, (3, 0, 1, 2))
+    x_val = np.transpose(x_val, (3, 0, 1, 2))
     x_test = np.transpose(x_test, (3, 0, 1, 2))
 
-    '''
-    trainMean = np.mean(x_train, axis=0)
-    print(trainMean)
-    trainSigma = np.std(x_train, axis=0)
-    print(trainSigma)
-    '''
-
-    trainMeanFile = h5py.File('SVHN_preprocessing.h5', 'r')
-    #trainMeanFile.create_dataset('train_mean', data=trainMean)
-    #trainMeanFile.create_dataset('train_sigma', data=trainSigma)
-    trainMean = np.array(trainMeanFile['train_mean'])
-    trainSigma = np.array(trainMeanFile['train_sigma'])
-    trainMeanFile.close()
-
-    x_train = (x_train - trainMean)/trainSigma
-    x_test = (x_test - trainMean)/trainSigma
-
     # convert class vectors to binary class matrices
-    y_train -= 1
-    y_test -= 1
     y_train = y_train.ravel()
+    y_val = y_val.ravel()
     y_test = y_test.ravel()
     y_train = keras.utils.np_utils.to_categorical(y_train, num_classes)
+    y_val = keras.utils.np_utils.to_categorical(y_val, num_classes)
     y_test = keras.utils.np_utils.to_categorical(y_test, num_classes)
 
-    return x_train, y_train, x_test, y_test
+    return x_train, y_train, x_val, y_val, x_test, y_test
--- SVHN/SVHN_CNN.py
+++ SVHN/SVHN_CNN.py
@@ -3,12 +3,16 @@ import sys
 sys.path.append('..')
 import keras
 from keras.optimizers import Adam, SGD
+from keras.layers import Dense, Conv2D, Concatenate, MaxPooling2D, GlobalAveragePooling2D, Input, BatchNormalization, Activation
+from keras.models import Model
+from denseBlocks import DenseTarget2D
 from denseBlocks import DenseNet, wideResNet
 from SVHNDataUtils import getSVHNData
 from visualization import *
 from keras.callbacks import LearningRateScheduler, CSVLogger, ModelCheckpoint
+np.random.seed(1337)
 
-batch_size = 128
+batch_size = 64
 epochs = 40
 
 def scheduler(epoch):
@@ -18,10 +22,54 @@ def scheduler(epoch):
         print("lr changed to {}".format(lr*.1))
     return K.get_value(model.optimizer.lr)
 
-x_train, y_train, x_test, y_test = getSVHNData()
+x_train, y_train, x_val, y_val, x_test, y_test = getSVHNData()
 
-model = wideResNet(k=8, dropout=0.4, include_target='false')
-#model = DenseNet(growth_rate=48, reduction=0.5, dropout_rate=0.2, include_target='false')
+#model = wideResNet(k=8, dropout=0.4, include_target='false')
+model = DenseNet(growth_rate=48, reduction=0.5, dropout_rate=0.2, include_target='false')
+
+'''
+growth_rate=48
+l2 = 0.001
+l2_buildup = 1
+
+input = Input(shape=(32,32,3))
+x = Conv2D(32, kernel_size=(3, 3), padding='same', use_bias=False)(input)
+x = BatchNormalization()(x)
+x = Activation('relu')(x)
+
+y = DenseTarget2D(x, growth_rate=growth_rate, include_target = 'true', l2=l2)
+x = Concatenate(axis=-1)([x, y])
+l2 *= l2_buildup
+
+y = DenseTarget2D(x, growth_rate=growth_rate, include_target = 'true', l2=l2)
+x = Concatenate(axis=-1)([x, y])
+l2 *= l2_buildup
+
+x = MaxPooling2D(pool_size=(2, 2))(x)
+
+y = DenseTarget2D(x, growth_rate=growth_rate, include_target = 'true', l2=l2)
+x = Concatenate(axis=-1)([x, y])
+l2 *= l2_buildup
+
+y = DenseTarget2D(x, growth_rate=growth_rate, include_target = 'true', l2=l2)
+x = Concatenate(axis=-1)([x, y])
+l2 *= l2_buildup
+
+x = MaxPooling2D(pool_size=(2, 2))(x)
+
+y = DenseTarget2D(x, growth_rate=growth_rate, include_target = 'true', l2=l2)
+x = Concatenate(axis=-1)([x, y])
+l2 *= l2_buildup
+
+y = DenseTarget2D(x, growth_rate=growth_rate, include_target = 'true', l2=l2)
+x = Concatenate(axis=-1)([x, y])
+
+x = BatchNormalization()(x)
+x = Activation('relu')(x)
+x = GlobalAveragePooling2D()(x)
+out = Dense(10, activation='softmax')(x)
+model = Model(input, out)
+'''
 
 # Optimizer
 # adam = Adam(lr= 0.01, beta_1= 0.9, beta_2= 0.999, epsilon= 1e-08, decay= 0.0)
@@ -45,9 +93,13 @@ model.fit(x_train, y_train,
           epochs=epochs,
           verbose=1,
           callbacks=callbacks_list,
-          validation_data=(x_test, y_test))
+          validation_data=(x_val, y_val))
+
+score = model.evaluate(x_test, y_test)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
 
 # visualization of the gaussian filters
-# visualizeLayerOutput(model, 10, 4, 4)
-# visualizeLayerOutput(model, 16, 4, 4)
-# visualizeLayerOutput(model, 22, 4, 4)
+# visualizeLayerOutput(model, 8, 6, 4)
+# visualizeLayerOutput(model, 13, 6, 4)
+# visualizeLayerOutput(model, 18, 6, 4)
